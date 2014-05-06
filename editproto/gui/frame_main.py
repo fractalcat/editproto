1777bf9ffc4b222eb5c2f4591e588905b56f6b4c
Sharif Olorin <sio@tesser.org>
2014-05-06 19:41:06 +1000

    Let's actually have a frame

--- editproto/gui/frame_main.py
+++ editproto/gui/frame_main.py
@@ -2,6 +2,6 @@ import wx
 
 class MainFrame(wx.Frame):
     def __init__(self, parent, id, opts):
-        wx.Frame.__init__(self, parent)
+        wx.Frame.__init__(self, parent, id, "editproto")
         self.Show(True)
         self.Maximize(True)
