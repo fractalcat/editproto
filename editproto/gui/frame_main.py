8f515823274c94e65a294550340e8e8d14500cf4
Sharif Olorin <sio@tesser.org>
2015-03-13 06:22:04 +0000

    Fuck it, write it in C++

--- editproto/gui/frame_main.py
+++
@@ -1,7 +0,0 @@
-import wx
-
-class MainFrame(wx.Frame):
-    def __init__(self, parent, id, opts):
-        wx.Frame.__init__(self, parent, id, "editproto")
-        self.Show(True)
-        self.Maximize(True)
